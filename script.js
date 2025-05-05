fe8b71bcd008e4fccb11e51a0ca54d20011da449
prabhakaranragh <prabhakaranragh@gmail.com>
2025-05-05 09:14:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
 const quotes = [
-  {
-    
-    image: "image1.png"
-  },
-  {
-    
-    image: "image2.png"
-  },
-  {
-    
-    image: "image3.png"
-  },
-  {
-    
-    image: "image4.png"
-  },
-  {
-    
-    image: "image5.png"
-  }
+  { image: "image1.png" },
+  { image: "image2.png" },
+  { image: "image3.png" },
+  { image: "image4.png" },
+  { image: "image5.png" }
 ];
 
 function generateQuote() {
   const random = Math.floor(Math.random() * quotes.length);
-  
   document.getElementById("quote-image").src = quotes[random].image;
 }
 
 window.onload = generateQuote;
+
