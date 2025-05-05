a767a704185ca2614fa3eaa0aae8018100c44afd
prabhakaranragh <prabhakaranragh@gmail.com>
2025-05-05 09:02:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const quotes = [
   {
-    text: "STAY CALM.\nSTAY FOCUSED.\nSTAY UNSTOPPABLE.",
+    
     image: "image1.png"
   },
   {
-    text: "HUSTLE IN SILENCE.\nLET SUCCESS MAKE THE NOISE.",
+    
     image: "image2.png"
   },
   {
-    text: "THE JOURNEY MAY BE LONG, BUT EVERY STEP COUNTS.",
+    
     image: "image3.png"
   },
   {
-    text: "EVERY SUNRISE IS A NEW OPPORTUNITY TO RISE ABOVE YESTERDAY.",
+    
     image: "image4.png"
   },
   {
-    text: "DOUBT KILLS MORE DREAMS THAN FAILURE EVER WILL.",
+    
     image: "image5.png"
   }
 ];
